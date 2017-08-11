67ba4654f2dc09fa8390a09929405257fcc8fa02
Zain Mehdi <szainmehdi@gmail.com>
2017-08-11 14:18:14 -0700

    Add environment variables for client ID and domains.

---
+++ .env.example.js
@@ -0,0 +1,6 @@
+// Note: string variables need to be wrapped into single and double quotes '"..."'
+module.exports = {
+  API_DOMAIN: '"http://api.nawhas.app"',
+  APP_DOMAIN: '"http://nawhas.app"',
+  API_CLIENT_ID: 1
+};
