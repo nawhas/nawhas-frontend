8938c781b1095fd28b9559b3815ba6a4277763dc
Asif Ali <shea786@live.co.uk>
2017-09-03 23:52:51 +0000

    added stores for tracks and added test button for tracks on reciter vue, also changed structure and outputting all reciters, albums for nadeem sarwar and tracks for 2009

--- src/views/public/Reciters.vue
+++ src/views/public/Reciters.vue
@@ -3,9 +3,15 @@
     <h3>Reciters Page</h3>
     <v-btn @click="loadReciters()">Load Reciters</v-btn>
     <v-btn @click="loadAlbums()">Load Albums</v-btn>
+    <v-btn @click="loadTracks()">Load Tracks</v-btn>
     <div v-for="reciter in reciters" :key="reciter.id">
       <p>{{ reciter.name }}</p>
-      <p v-for="album in albums">{{ album.name }}</p>
+      <div v-for="album in albums" :key="album.id">
+        <p>{{ album.name }}</p>
+        <div v-for="track in tracks" :key="track.id">
+          <p >{{ track.name }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,6 +25,9 @@ export default {
     },
     loadAlbums() {
       this.$store.dispatch('albums/fetchAlbums');
+    },
+    loadTracks() {
+      this.$store.dispatch('tracks/fetchTracks');
     }
   },
   computed: {
@@ -27,6 +36,9 @@ export default {
     },
     albums() {
       return this.$store.state.albums.collection;
+    },
+    tracks() {
+      return this.$store.state.tracks.collection;
     }
   }
 };
